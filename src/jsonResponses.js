3cbbd8dc7051950825539443f84f36761de34dea
Michael Chan (RIT Student) <mhc4466@rit.edu>
2022-09-12 13:54:34 -0400

    Finished class demo

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -17,6 +17,14 @@ const badRequest = (request, response, params) => {
     message: 'This request has the required parameters',
   };
 
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'Missing valid query param equal to true';
+    responseJSON.id = 'badRequestMissingParam';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  else {
+    return respondJSON(request, response, 200, responseJSON);
+  }
 };
 
 const notFound = (request, response) => {
@@ -24,7 +32,7 @@ const notFound = (request, response) => {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
   };
-
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
+const { parse } = require('path');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -14,7 +15,16 @@ const urlStruct = {
 };
 
 const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
 
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  }
+  else {
+    urlStruct.notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
